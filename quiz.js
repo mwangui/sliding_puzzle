84a042c796557472a7f3fc95d248ea213cf343d4
Ming Wang <ming.wang@spigit.com>
2019-11-11 11:36:08 -0800

    fix bugs

--- quiz.js
+++ quiz.js
@@ -1,75 +1,86 @@
-
-// note: I still have some bugs need to fix.
-// if you allow me more time, I will fix the move up click issue
-// thanks.
-
 window.onload = function() {
     const buttons = document.querySelectorAll('.Tile');
     const space = document.getElementsByClassName('Tile')[0].offsetWidth;
     let temp = [1,1,1,1,1,1,1,1,0];
-    let currentButton;
-    let btnBeenClick;
-    let perNum = 0;
-    let nextNum = 0;
-    let topNum = 0;
+    let startPoint = [[0,0],[0,0], [0,0], [0,0], [0,0], [0,0],[0,0], [0,0], [0,0]];
+    let thisBtnIndex;
+    let perIndex = 0;
+    let nextIndex = 0;
+    let topNumberIndex = 0;
     let bottomNum = 0;
     let clickTime = 0;
+    let afterMoveXPosition;
+    let className;
+    let classNumber;
+
 
     if ( clickTime == 1) {
-        temp[8] = 1;
+        temp[temp.length - 1] = 1;
+    }
+
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].setAttribute('data-id' , i);
     }
 
     for (const button of buttons) {
         button.addEventListener('click', function(event) {
             clickTime++;
+
+            // to get the button index
+            className = event.path[0].className;
+            classNumber = className.charAt(className.length-1);
+
             //to get the current button number
-            currentButton = event.path[0].className.charAt(event.path[0].className.length -1);
-            perNum = parseFloat(currentButton) - 2;
-            btnBeenClick = parseFloat(currentButton) -1;
-            nextNum = parseFloat(currentButton);
-            topNum = parseFloat(currentButton) - 4 ;
-            bottomNum = parseFloat(currentButton) + 2;
+            thisBtnIndex = event.path[0].dataset.id;
+            perIndex = parseFloat(thisBtnIndex) - 1;
+            nextIndex = parseFloat(thisBtnIndex) + 1;
+            topNumberIndex = parseFloat(thisBtnIndex) - 3 ;
+            bottomNum = parseFloat(thisBtnIndex) + 3;
 
             for (let i = 0; i <= temp.length; i++) {
-                if ( temp[perNum] == 0) {
-                    temp[btnBeenClick] = 0;
-                    temp[perNum] = 1;
-                    moveToLeft(currentButton, space, event);
-
-                } if ( temp[nextNum] == 0) {
-                    temp[btnBeenClick] = 0;
-                    temp[nextNum] = 1;
-                    moveToRight(currentButton, space, event);
-
-                } if ( temp[topNum]  == 0 || temp[topNum] <0 ) {
-                    temp[btnBeenClick] = 0;
-                    temp[topNum] = 1;
-                    moveToTop(currentButton, space, event);
-
+                if ( temp[perIndex] == 0) {
+                    temp[thisBtnIndex] = 0;
+                    temp[perIndex] = 1;
+                    event.path[0].dataset.id = perIndex;
+                    moveToLeft(event, space, startPoint, afterMoveXPosition, classNumber);
+                } if ( temp[nextIndex] == 0) {
+                    temp[thisBtnIndex] = 0;
+                    temp[nextIndex] = 1;
+                    event.path[0].dataset.id = nextIndex;
+                    moveToRight(event, space, startPoint, afterMoveXPosition, classNumber);
+                } if ( temp[topNumberIndex]  == 0  ) {
+                    temp[thisBtnIndex] = 0;
+                    temp[topNumberIndex] = 1;
+                    event.path[0].dataset.id = topNumberIndex;
+                    moveToTop(event, space, startPoint, afterMoveXPosition, classNumber);
                 } if (temp[bottomNum] == 0) {
-                    temp[btnBeenClick] = 0;
+                    temp[thisBtnIndex] = 0;
                     temp[bottomNum] = 1;
-                    moveToBottom(currentButton, space, event);
+                    event.path[0].dataset.id = bottomNum;
+                    moveToBottom(event, space, startPoint, afterMoveXPosition, classNumber);
                 }
             }
         });
-    };
+    }
 };
 
 
-function moveToLeft(currentButton, space, event) {
-    event.target.style.left = - space + "px";
+function moveToLeft(event, space, startPoint, afterMoveXPosition, classNumber) {
+    afterMoveXPosition = startPoint[classNumber -1][0] -= space;
+    event.target.style.left = afterMoveXPosition + 'px';
 }
 
-function moveToRight(currentButton, space, event) {
-    event.target.style.left = + space + "px";
+function moveToRight(event, space, startPoint, afterMoveXPosition, classNumber) {
+    afterMoveXPosition = startPoint[classNumber -1][0] += space;
+    event.target.style.left = afterMoveXPosition + 'px';
 }
 
-function moveToTop(currentButton, space, event) {
-    event.target.style.top = - space + "px";
+function moveToTop(event, space, startPoint, afterMoveXPosition, classNumber) {
+    afterMoveXPosition = startPoint[classNumber -1][1] -= space;
+    event.target.style.top = afterMoveXPosition + 'px';
 }
 
-function moveToBottom(currentButton, space, event) {
-    event.target.style.top = + space + "px";
+function moveToBottom(event, space, startPoint, afterMoveXPosition, classNumber) {
+    afterMoveXPosition = startPoint[classNumber -1][1] += space;
+    event.target.style.top = afterMoveXPosition + 'px';
 }
-
